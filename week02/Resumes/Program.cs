26aa3a6f0a16b4f850e224aa35911c3cc765c02a
TonyReignsT <antonyochieng00@gmail.com>
2025-09-13 10:23:36 +0300

    Created a Job class

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Project Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+
+        job2.Display();
     }
 }
